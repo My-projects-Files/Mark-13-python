70a84966f5c85923cae26ae9ebc0adf5906a5432
saikamal33 <kamaleshsai33@gmail.com>
2025-03-20 16:48:50 +0530

    pyhton

---
+++ python-2/float-inbuild-function.py
@@ -0,0 +1,16 @@
+#float variables
+num1=5.0
+num2=2.5
+
+# Basic arithmetic
+result1= num1+num2
+print("adding:",result1)
+
+result2= num1-num2
+print("subtract:", result2)
+
+result3= num1 * num2
+print("multiply:",result3)
+
+result4= num1/num2
+print("divide:",result4)
