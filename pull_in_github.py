ebb2084da245a92a91a7a1669a4315ef5b3ea3d9
saikamal33 <kamaleshsai33@gmail.com>
2025-03-28 17:09:27 +0530

    dictionary

--- pull_in_github.py
+++ pull_in_github.py
@@ -1,3 +1,22 @@
 import requests
 
-"api.github.com/repos/kubernetes/kubernetes/pulls"
+url=f"https://api.github.com/repos/kubernetes/kubernetes/pulls"
+
+response=requests.get(url)
+status= response.status_code
+
+if status == 200:
+    complete=response.json()
+    pull_cr={}
+    for i in complete:
+        cr= i ['user']['login']
+        if cr in pull_cr:
+            pull_cr[cr]+=1
+        else:
+            pull_cr[cr]=1
+    print(pull_cr)
+    print("pull request created and counts:")
+    for cr,count in pull_cr.items():
+        print(f"pull requests for {cr}:{count}")
+else:
+    print(f"failed to fetch data.error code:{status}")
